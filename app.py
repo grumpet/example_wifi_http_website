c419e96ad32448e8ed097eebd4c02b56557ab6d4
nimrosh <nimrodka@mta.ac.il>
2024-11-12 15:06:04 +0200

    ADDED WEBSOCKET

--- app.py
+++ app.py
@@ -7,11 +7,14 @@ import io
 import base64
 from dotenv import load_dotenv
 import os
-#loads the environment variables from the .env file
+from flask_socketio import SocketIO, emit
+
+# Load environment variables
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+socketio = SocketIO(app)
 
 # In-memory storage for user preferences
 user_data = []
@@ -20,6 +23,20 @@ class UserForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     preference = RadioField('Do you prefer:', choices=[('dogs', 'Dogs'), ('cats', 'Cats')], validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+def generate_pie_chart():
+    dog_count = sum(1 for user in user_data if user['preference'] == 'dogs')
+    cat_count = sum(1 for user in user_data if user['preference'] == 'cats')
+    labels = 'Dogs', 'Cats'
+    sizes = [dog_count, cat_count]
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+    ax.axis('equal')
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return plot_url
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -33,6 +50,9 @@ def home():
                 return redirect(url_for('results'))
         user_data.append({'name': name, 'preference': preference})
         session['name'] = name
+        # Emit the updated data to all connected clients
+        plot_url = generate_pie_chart()
+        socketio.emit('update_results', {'user_data': user_data, 'plot_url': plot_url})
         return redirect(url_for('results'))
     return render_template('home.html', form=form)
 
@@ -41,20 +61,8 @@ def results():
     if 'name' not in session:
         return redirect(url_for('home'))
     
-    # Generate pie chart
-    dog_count = sum(1 for user in user_data if user['preference'] == 'dogs')
-    cat_count = sum(1 for user in user_data if user['preference'] == 'cats')
-    labels = 'Dogs', 'Cats'
-    sizes = [dog_count, cat_count]
-    fig, ax = plt.subplots()
-    ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
-    ax.axis('equal')
-    img = io.BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-    plot_url = base64.b64encode(img.getvalue()).decode()
-
+    plot_url = generate_pie_chart()
     return render_template('results.html', plot_url=plot_url, user_data=user_data)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    socketio.run(app, debug=True, host='0.0.0.0')
